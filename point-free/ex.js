fb3dde0d323a0ecd3bae4bcfa9645ab7b3b9cc16
Maja <mkudlicka@bamboorose.com>
2025-08-02 11:08:18 +0200

    feat: solution for the point-free exercise

--- point-free/ex.js
+++ point-free/ex.js
@@ -1,27 +1,33 @@
 "use strict";
 
-function output(txt) {
-	console.log(txt);
-}
-
-function printIf(shouldPrintIt) {
-	return function(msg) {
-		if (shouldPrintIt(msg)) {
-			output(msg);
-		}
+function not(fn) {
+	return function(...args) {
+		return !fn(...args);
 	};
 }
+
+const output = console.log.bind(console);
+
+function when(fn) {
+	return function(predicate){
+		return function(...args){
+			if (predicate(...args)) {
+				return fn(...args);
+			}
+		};
+	};
+}
+
+const printIf = when(output);
 
 function isShortEnough(str) {
 	return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
-}
+const isLongEnough = not(isShortEnough);
 
-var msg1 = "Hello";
-var msg2 = msg1 + " World";
+const msg1 = "Hello";
+const msg2 = msg1 + " World";
 
 printIf(isShortEnough)(msg1);		// Hello
 printIf(isShortEnough)(msg2);
