a4cf14dfb1206d972ecd93a2b18e89deedec1219
Maja <mkudlicka@bamboorose.com>
2025-08-06 14:33:59 +0200

    feat: added immutability exercies solution

--- immutability/ex.js
+++ immutability/ex.js
@@ -4,12 +4,21 @@ function lotteryNum() {
 	return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber() {}
+function pickNumber(randomNum, array) {
+	if (!array.includes(randomNum)) {
+		array = [...array, randomNum]
+		array.sort((a, b) => {
+			return a - b
+		})
+	}
+	return array
+}
 
 var luckyLotteryNumbers = [];
+const lotteryNumCount = 6;
 
-while (luckyLotteryNumbers.length < 6) {
-	pickNumber();
+while (luckyLotteryNumbers.length < lotteryNumCount) {
+	luckyLotteryNumbers = pickNumber(lotteryNum(), luckyLotteryNumbers);
 }
 
 console.log(luckyLotteryNumbers);
