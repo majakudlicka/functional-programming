bfa7d28f6af5833474661b9cb10dc8fd497b2c05
Maja <mkudlicka@bamboorose.com>
2025-08-07 10:27:27 +0200

    feat: add recursion exercise solutiojn

--- recursion/ex.js
+++ recursion/ex.js
@@ -1,6 +1,10 @@
 "use strict";
 
-function isPalindrome() {}
+function isPalindrome(str) {
+    if (str.length <= 1) return true
+    if (str[0] !== str[str.length -1]) return false
+    return isPalindrome(str.slice(1, -1))
+}
 
 console.log( isPalindrome("") === true );
 console.log( isPalindrome("a") === true );
